d61643267336f22cb89191d0f5b377ae79943aa2
hasansayeed-coder <hasansayeed791@gmail.com>
2023-06-03 11:29:15 +0600

    [Done] Setup cors and parser

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function bootstrap() {
+
+    try{
+
+        await mongoose.connect(config.database_url as string);
+        console.log("database connected for university management") ;
+        
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+          })
+
+    }catch(err){
+        console.log("FAiled " , err); 
+    }
+    
+  }
+
+  bootstrap() ;
